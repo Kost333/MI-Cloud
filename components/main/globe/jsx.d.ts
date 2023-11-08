3ed767239cdf229cb8a43338bd4b16bc9ba1d940
Kost333 <levonkostanyan2021@gmail.com>
2023-11-08 18:03:06 +0400

    delete globe

--- components/main/globe/jsx.d.ts
+++
@@ -1,8 +0,0 @@
-import React, {AriaAttributes, DOMAttributes} from 'react';
-
-declare module 'react' {
-    interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
-        // Extend the HTMLAttributes interface to include model-viewer element
-        'model-viewer'?: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-    }
-}
